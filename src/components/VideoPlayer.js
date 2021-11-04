6350fc0bc9f7fed91fa50fb03d22796999b60c24
amalvarghese593 <amalvarghese593@gmail.com>
2021-11-04 11:34:45 +0530

    logos organised

--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -1,6 +1,13 @@
 import React, { useRef, useEffect, useState } from "react";
 import "./videoplayer.css";
 import vid from "../video/sintel-short.mp4";
+import play from "../logos/play.svg";
+import pause from "../logos/pause.svg";
+import full from "../logos/full.svg";
+import back from "../logos/back.svg";
+import exitFull from "../logos/exitFull.svg";
+import thumbnail from "../logos/thumbnail.svg";
+import forward from "../logos/forward.svg";
 
 export const VideoPlayer = () => {
   const vidRef = useRef();
@@ -110,18 +117,7 @@ export const VideoPlayer = () => {
         className="rc-video"
       >
         <div className="thumbnail-svg" style={thmb}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="66"
-            height="66"
-            color="#ff6d25"
-            fill="currentColor"
-            class="bi bi-play-btn-fill"
-            viewBox="0 0 16 16"
-            onClick={thumbFtn}
-          >
-            <path d="M0 12V4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm6.79-6.907A.5.5 0 0 0 6 5.5v5a.5.5 0 0 0 .79.407l3.5-2.5a.5.5 0 0 0 0-.814l-3.5-2.5z" />
-          </svg>
+          <img src={thumbnail} alt="thumbnail logo" onClick={thumbFtn} />
         </div>
         <video
           onTimeUpdate={fn1}
@@ -140,100 +136,35 @@ export const VideoPlayer = () => {
             <div className="c-bar" onClick={seekFn}>
               <div ref={barRef} id="Bar" className="orange-bar"></div>
             </div>
-
             <div className="btn">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                onClick={mediaBackward}
-                color="#fff"
-                width="36"
-                height="36"
-                fill="currentColor"
-                class="bi bi-arrow-counterclockwise"
-                viewBox="0 0 16 16"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M8 3a5 5 0 1 1-4.546 2.914.5.5 0 0 0-.908-.417A6 6 0 1 0 8 2v1z"
-                />
-                <path d="M8 4.466V.534a.25.25 0 0 0-.41-.192L5.23 2.308a.25.25 0 0 0 0 .384l2.36 1.966A.25.25 0 0 0 8 4.466z" />
-              </svg>
+              <img src={back} alt="back logo" onClick={mediaBackward} />
               {flag === false ? (
-                <svg
+                <img
+                  src={play}
+                  alt="play logo"
                   onClick={() => {
                     setFlag(!flag);
                   }}
-                  xmlns="http://www.w3.org/2000/svg"
-                  color="#fff"
-                  width="36"
-                  height="36"
-                  fill="currentColor"
-                  class="bi bi-play-circle"
-                  viewBox="0 0 16 16"
-                >
-                  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
-                  <path d="M6.271 5.055a.5.5 0 0 1 .52.038l3.5 2.5a.5.5 0 0 1 0 .814l-3.5 2.5A.5.5 0 0 1 6 10.5v-5a.5.5 0 0 1 .271-.445z" />
-                </svg>
-              ) : (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  onClick={() => {
-                    setFlag(!flag);
-                  }}
-                  color="#fff"
-                  width="36"
-                  height="36"
-                  fill="currentColor"
-                  class="bi bi-pause-circle"
-                  viewBox="0 0 16 16"
-                >
-                  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
-                  <path d="M5 6.25a1.25 1.25 0 1 1 2.5 0v3.5a1.25 1.25 0 1 1-2.5 0v-3.5zm3.5 0a1.25 1.25 0 1 1 2.5 0v3.5a1.25 1.25 0 1 1-2.5 0v-3.5z" />
-                </svg>
-              )}
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                onClick={mediaForward}
-                color="#fff"
-                width="36"
-                height="36"
-                fill="currentColor"
-                class="bi bi-arrow-clockwise"
-                viewBox="0 0 16 16"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2v1z"
                 />
-                <path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466z" />
-              </svg>
-
+              ) : (
+                <img
+                  src={pause}
+                  alt="pause logo"
+                  onClick={() => {
+                    setFlag(!flag);
+                  }}
+                />
+              )}
+              <img src={forward} alt="forward logo" onClick={mediaForward} />
               <div className="time-c">
                 {flag2 === true ? (
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    onClick={fullFn}
-                    color="#fff"
-                    width="26"
-                    height="26"
-                    fill="currentColor"
-                    class="bi bi-fullscreen"
-                    viewBox="0 0 16 16"
-                  >
-                    <path d="M1.5 1a.5.5 0 0 0-.5.5v4a.5.5 0 0 1-1 0v-4A1.5 1.5 0 0 1 1.5 0h4a.5.5 0 0 1 0 1h-4zM10 .5a.5.5 0 0 1 .5-.5h4A1.5 1.5 0 0 1 16 1.5v4a.5.5 0 0 1-1 0v-4a.5.5 0 0 0-.5-.5h-4a.5.5 0 0 1-.5-.5zM.5 10a.5.5 0 0 1 .5.5v4a.5.5 0 0 0 .5.5h4a.5.5 0 0 1 0 1h-4A1.5 1.5 0 0 1 0 14.5v-4a.5.5 0 0 1 .5-.5zm15 0a.5.5 0 0 1 .5.5v4a1.5 1.5 0 0 1-1.5 1.5h-4a.5.5 0 0 1 0-1h4a.5.5 0 0 0 .5-.5v-4a.5.5 0 0 1 .5-.5z" />
-                  </svg>
+                  <img src={full} alt="full logo" onClick={fullFn} />
                 ) : (
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
+                  <img
+                    src={exitFull}
+                    alt="exitfull logo"
                     onClick={exitFullFn}
-                    width="26"
-                    height="26"
-                    fill="currentColor"
-                    class="bi bi-fullscreen-exit"
-                    viewBox="0 0 16 16"
-                  >
-                    <path d="M5.5 0a.5.5 0 0 1 .5.5v4A1.5 1.5 0 0 1 4.5 6h-4a.5.5 0 0 1 0-1h4a.5.5 0 0 0 .5-.5v-4a.5.5 0 0 1 .5-.5zm5 0a.5.5 0 0 1 .5.5v4a.5.5 0 0 0 .5.5h4a.5.5 0 0 1 0 1h-4A1.5 1.5 0 0 1 10 4.5v-4a.5.5 0 0 1 .5-.5zM0 10.5a.5.5 0 0 1 .5-.5h4A1.5 1.5 0 0 1 6 11.5v4a.5.5 0 0 1-1 0v-4a.5.5 0 0 0-.5-.5h-4a.5.5 0 0 1-.5-.5zm10 1a1.5 1.5 0 0 1 1.5-1.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 0-.5.5v4a.5.5 0 0 1-1 0v-4z" />
-                  </svg>
+                  />
                 )}
                 <div className="time-bar">
                   <span className="timeLapsed">{timeLapsed}</span> /&nbsp;
